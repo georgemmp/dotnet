cef15cbd23773fa5655f280b90fe554f997d19d3
georgemmp <machadogeorge.p@gmail.com>
2019-07-23 16:56:57 -0300

    Alteração no metodo AddPhotoForUser

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -52,7 +52,7 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddPhotoForUser(int userId, PhotoForCreationDto photo)
+        public async Task<IActionResult> AddPhotoForUser(int userId, [FromForm] PhotoForCreationDto photo)
         {
             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
             {
